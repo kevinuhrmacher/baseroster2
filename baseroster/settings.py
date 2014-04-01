703706d565f1faef04ad2cf2724e1f3a3097009a
kevinuhrmacher <kevin.uhrmacher@gmail.com>
2014-04-01 16:17:50 -0400

    database change

--- baseroster/settings.py
+++ baseroster/settings.py
@@ -112,7 +112,7 @@ TEMPLATE_LOADERS = (
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config(default=os.environ["HEROKU_POSTGRESQL_ROSE_URL"])
+DATABASES['default'] =  dj_database_url.config(default=os.environ["HEROKU_POSTGRESQL_TEAL_URL"])
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
